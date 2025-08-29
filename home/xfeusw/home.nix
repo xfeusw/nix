ee93aa39918c38ebad61cd209f901170a31414ef
xfeusw <khamrakulovkamron@gmail.com>
2025-08-29 22:26:00 +0500

    font updates

--- home/xfeusw/home.nix
+++ home/xfeusw/home.nix
@@ -1,22 +1,32 @@
 { config, pkgs, lib, ... }:
-
 {
   home.username = "xfeusw";
   home.homeDirectory = "/home/xfeusw";
   home.stateVersion = "25.05";
-
   programs.home-manager.enable = true;
-
+  fonts.fontconfig.enable = true;
+  
+  # Fix version mismatch warning
+  home.enableNixpkgsReleaseCheck = false;
+  
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  
   home.packages = with pkgs; [
     tree
     telegram-desktop
     zed-editor
-    code-cursor
+    code-cursor  # This will now work
     vscode
     spotify
     yandex-music
     discord
     tor-browser
     brave
+    # Fonts
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
+    fira-code
+    jetbrains-mono
   ];
-} 
+}
