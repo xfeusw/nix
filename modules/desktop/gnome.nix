549211c67241929a38e087bfe04b77701a4ceca3
xfeusw <khamrakulovkamron@gmail.com>
2025-08-29 19:48:33 +0500

    Modularize NixOS config: add modules and host imports

---
+++ modules/desktop/gnome.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+
+{
+  services.xserver.enable = true;
+
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  services.xserver.xkb.layout = "us";
+  services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  services.libinput.enable = true;
+
+  programs.firefox.enable = true;
+} 
