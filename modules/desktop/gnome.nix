9785dea1bd9a4c934050efb34e5c6585cc324102
xfeusw <khamrakulovkamron@gmail.com>
2025-08-29 20:20:12 +0500

    desktop: add Russian layout and Alt+Shift/Win+Space toggles

--- modules/desktop/gnome.nix
+++ modules/desktop/gnome.nix
@@ -1,20 +1,24 @@
 { config, lib, pkgs, ... }:
 
 {
+  # X11 + GNOME
   services.xserver.enable = true;
-
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
-  services.xserver.xkb.layout = "us";
-  services.xserver.xkb.options = "eurosign:e,caps:escape";
+  # Keyboard
+  services.xserver.xkb.layout = "us,ru";
+  services.xserver.xkb.options = "eurosign:e,caps:escape,grp:alt_shift_toggle,grp:win_space_toggle";
 
+  # Audio
   services.pipewire = {
     enable = true;
     pulse.enable = true;
   };
 
+  # Input
   services.libinput.enable = true;
 
+  # Apps
   programs.firefox.enable = true;
 } 
