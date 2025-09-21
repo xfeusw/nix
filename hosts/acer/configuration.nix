b936bb4c7f54d873e245059bafc3c07b26041e5d
xfeusw <khamrakulovkamron@gmail.com>
2025-09-22 01:54:10 +0500

    men pizdes bo'lganman

--- hosts/acer/configuration.nix
+++ hosts/acer/configuration.nix
@@ -9,50 +9,68 @@
     ../../modules/networking.nix
     ../../modules/users.nix
     ../../modules/security.nix
-    ../../modules/power.nix
+    ../../modules/performance.nix
     ../../modules/backup.nix
 
     # Desktop environment
     ../../modules/desktop/gnome.nix
 
-    # Modular imports (new structure)
-    ../../modules/packages # All package categories
-    ../../modules/hardware # All hardware configurations
-    ../../modules/services # All services
-    ../../modules/virtualization # All virtualization
-
-    # Hardware-specific (uncomment as needed)
-    # inputs.nixos-hardware.nixosModules.common-cpu-intel
-    # inputs.nixos-hardware.nixosModules.common-pc-ssd
+    # Modular imports
+    ../../modules/packages
+    ../../modules/hardware
+    ../../modules/services
+    ../../modules/virtualization
   ];
 
   # Bootloader with enhanced options
   boot = {
     loader = {
-      systemd-boot.enable = true;
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 10;  # Limit boot entries
+        editor = false;  # Security: disable boot editor
+      };
       efi.canTouchEfiVariables = true;
-      timeout = 3;
+      timeout = 2;  # Reduced timeout
     };
 
     # Kernel optimizations
     kernelParams = [
       "quiet"
       "splash"
-      "mitigations=off" # Better performance, slightly less security
+      "loglevel=3"
+      "systemd.show_status=auto"
+      "rd.udev.log_level=3"
     ];
 
-    kernel.sysctl = {
-      "vm.swappiness" = 10;
-      "vm.dirty_ratio" = 5;
-      "vm.dirty_background_ratio" = 2;
-      "net.core.rmem_max" = 16777216;
-      "net.core.wmem_max" = 16777216;
-    };
+    # Faster boot
+    initrd.verbose = false;
+    consoleLogLevel = 0;
+    kernelModules = [ "tcp_bbr" ];  # Better congestion control
   };
 
   # Timezone and localization
   time.timeZone = "Asia/Samarkand";
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    supportedLocales = [
+      "en_US.UTF-8/UTF-8"
+      "ru_RU.UTF-8/UTF-8"
+    ];
+  };
+
+  # Secrets management
+  sops = {
+    defaultSopsFile = ./secrets.yaml;
+    age.keyFile = "/home/xfeusw/.config/sops/age/keys.txt";
+    secrets = {
+      user-password.neededForUsers = true;
+      restic-password = {
+        owner = "root";
+        mode = "0600";
+      };
+    };
+  };
 
   # NixOS compatibility version
   system.stateVersion = "25.05";
