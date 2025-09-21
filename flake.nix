a522b4da1ddd56d42ed40292819d14a7c1a89529
xfeusw <khamrakulovkamron@gmail.com>
2025-09-22 01:54:10 +0500

    men pizdes bo'lganman

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-# flake.nix
 {
   description = "Enhanced NixOS configuration (flake) for acer";
 
@@ -6,6 +5,9 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
+
+    # Utils for cleaner code
+    flake-utils.url = "github:numtide/flake-utils";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
@@ -18,14 +20,38 @@
     # Additional useful inputs
     nur.url = "github:nix-community/NUR";
 
+    # State management
+    impermanence.url = "github:nix-community/impermanence";
+
+    # Consistent theming
+    nix-colors.url = "github:misterio77/nix-colors";
+
+    # Firefox extensions
+    firefox-addons = {
+      url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     # Your personal nixvim configuration
     nixvim-config = {
       url = "github:Ahwxorg/nixvim-config";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Secrets management
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Hyprland (optional Wayland compositor)
+    hyprland = {
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = {
+  outputs = inputs @ {
     nixpkgs,
     nixpkgs-unstable,
     nixpkgs-wayland,
@@ -33,48 +59,57 @@
     nixos-hardware,
     nur,
     nixvim-config,
+    impermanence,
+    nix-colors,
+    firefox-addons,
+    sops-nix,
+    flake-utils,
+    hyprland,
     ...
   }: {
     nixosConfigurations = {
       acer = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = {
-          inputs = {
-            inherit
-              nixpkgs
-              nixpkgs-unstable
-              nixpkgs-wayland
-              home-manager
-              nixos-hardware
-              nur
-              nixvim-config
-              ;
-          };
-          inherit
-            nixpkgs
-            nixpkgs-unstable
-            nixpkgs-wayland
-            home-manager
-            nixos-hardware
-            nur
-            nixvim-config
-            ;
+          inherit inputs;
+          inherit nixpkgs-unstable nixpkgs-wayland;
         };
         modules = [
           ./hosts/acer/configuration.nix
-          nur.modules.nixos.default
-          # Inject nixpkgs-wayland overlay + Cachix cache
+          nixos-hardware.nixosModules.common-cpu-intel
+          nixos-hardware.nixosModules.common-pc-ssd
+          nixos-hardware.nixosModules.common-pc-laptop
+          nur.nixosModules.nur
+          sops-nix.nixosModules.sops
+
+          # Global overlays
           {
-            nixpkgs.overlays = [nixpkgs-wayland.overlay];
+            nixpkgs.overlays = [
+              nixpkgs-wayland.overlay
+              nur.overlay
+              # Custom overlay for optimizations
+              (final: prev: {
+                unstable = import nixpkgs-unstable {
+                  system = prev.system;
+                  config.allowUnfree = true;
+                };
+              })
+            ];
 
             nix.settings = {
               substituters = [
                 "https://cache.nixos.org/"
                 "https://nixpkgs-wayland.cachix.org"
+                "https://nix-community.cachix.org"
+                "https://hyprland.cachix.org"
+                "https://nixpkgs-unfree.cachix.org"
               ];
               trusted-public-keys = [
                 "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
                 "nixpkgs-wayland.cachix.org-1:4f5VsYvpWm4VE/KJioPp6TcbRZ3PgCWvu5Zf+HjMZ9o="
+                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+                "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+                "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
               ];
             };
           }
@@ -86,22 +121,24 @@
       xfeusw = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux;
         extraSpecialArgs = {
-          inherit
-            nixpkgs
-            nixpkgs-unstable
-            nixpkgs-wayland
-            nixvim-config
-            nur
-            ;
+          inherit inputs;
+          inherit nixpkgs-unstable nixpkgs-wayland nixvim-config nur nix-colors firefox-addons;
           unstable = import nixpkgs-unstable {
             system = "x86_64-linux";
             config.allowUnfree = true;
+            overlays = [ nur.overlay ];
           };
         };
         modules = [
           ./home/xfeusw/home.nix
-          nur.modules.homeManager.default
-          {nixpkgs.overlays = [nixpkgs-wayland.overlay];}
+          nur.homeManagerModules.nur
+          nix-colors.homeManagerModules.default
+          {
+            nixpkgs.overlays = [
+              nixpkgs-wayland.overlay
+              nur.overlay
+            ];
+          }
         ];
       };
     };
--- home/xfeusw/development/tools.nix
+++ home/xfeusw/development/tools.nix
@@ -2,59 +2,109 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    # Modern development tools
+    gitoxide          # Faster git clone
+    delta             # Better git diff
+    difftastic        # Syntax-aware diff
+    just              # Command runner
+    direnv            # Environment management
+    cachix            # Binary cache management
+
+    # Nix-specific tools
+    nix-fast-build    # Faster nix builds
+    nix-update        # Update nix packages
+    nurl              # Generate nix URLs
+
+    # Enhanced CLI tools
+    bat               # Better cat
+    eza               # Better ls
+    fd                # Better find
+    ripgrep           # Better grep
+    zoxide            # Smart cd
+    fzf               # Fuzzy finder
+    btop              # Better top
+    dust              # Better du
+    procs             # Better ps
+    hyperfine         # Benchmarking
+    tokei             # Code statistics
+
     # Build tools
     gcc
     cmake
     gnumake
     pkg-config
+    meson
+    ninja
 
-    # Debuggers
+    # Debuggers and profilers
     gdb
     valgrind
+    lldb
+    perf-tools
+    flamegraph
 
     # Version control
     git-lfs
     gh
     lazygit
     gitui
+    git-absorb
 
-    # Container tools
+    # Container and orchestration tools
     docker-compose
     podman-compose
     kubectl
     k9s
     helm
+    dive              # Docker image analyzer
+    ctop              # Container top
 
     # Database tools
     sqlite
     postgresql_16
+    redis
+    dbeaver-bin
 
-    # API tools
+    # API and network tools
     httpie
     curl
     wget
+    postman
+    insomnia
 
     # Documentation
     mdbook
-
-    # Performance tools
-    hyperfine # Benchmarking
-    flamegraph # Profiling
-
-    # Network tools
-    nmap
-    wireshark
-    tcpdump
+    hugo
 
     # Text processing
-    jq # JSON processor
-    yq # YAML processor
+    jq                # JSON processor
+    yq                # YAML processor
+    xmlstarlet        # XML processor
 
-    # File tools
+    # System tools
     file
     tree
     unzip
     p7zip
     findutils
+    patchelf
+    binutils
+
+    # Security tools
+    age               # Encryption
+    sops              # Secrets management
+
+    # Monitoring
+    htop
+    iotop
+    nethogs
+    bandwhich
   ];
+
+  # Direnv integration
+  programs.direnv = {
+    enable = true;
+    enableZshIntegration = true;
+    nix-direnv.enable = true;
+  };
 }
