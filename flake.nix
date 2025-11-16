666ae4071688b583a79ce960d9bcdbc3b41a221b
xfeusw <khamrakulovkamron@gmail.com>
2025-11-16 15:33:54 +0500

    major changes and hosts/xeon is implemented

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Enhanced NixOS configuration (flake) for acer";
+  description = "Enhanced NixOS configuration (flake) for acer and xeon";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
@@ -59,57 +59,104 @@
     sops-nix,
     spicetify-nix,
     ...
-  }: {
-    nixosConfigurations = {
-      acer = nixpkgs.lib.nixosSystem {
+  }:
+  let
+    # Common NixOS modules shared across all hosts
+    commonNixosModules = [
+      nur.modules.nixos.default
+      sops-nix.nixosModules.sops
+      {
+        nixpkgs.overlays = [
+          nur.overlays.default
+          (final: prev: {
+            unstable = import nixpkgs-unstable {
+              system = prev.system;
+              config.allowUnfree = true;
+            };
+            weston = prev.weston.overrideAttrs (old: {
+              mesonFlags = (old.mesonFlags or []) ++ ["-Dbackend-vnc=false"];
+            });
+          })
+        ];
+
+        nixpkgs.config = {
+          allowUnfree = true;
+          nvidia.acceptLicense = true;
+        };
+
+        nix.settings = {
+          substituters = [
+            "https://cache.nixos.org/"
+            "https://nix-community.cachix.org"
+            "https://hyprland.cachix.org"
+            "https://nixpkgs-unfree.cachix.org"
+          ];
+          trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+            "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+            "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+          ];
+        };
+      }
+    ];
+
+    # Common home-manager modules
+    commonHomeModules = [
+      plasma-manager.homeModules.plasma-manager
+      nix-colors.homeManagerModules.default
+      niri.homeModules.niri
+      niri.homeManagerModules.default
+      sops-nix.homeModules.sops
+      spicetify-nix.homeManagerModules.default
+      {
+        nixpkgs.config.allowUnfree = true;
+        nixpkgs.overlays = [
+          nur.overlays.default
+          (final: prev: {
+            unstable = import nixpkgs-unstable {
+              system = prev.system;
+              config.allowUnfree = true;
+            };
+          })
+        ];
+      }
+    ];
+
+    # Host-specific configurations
+    hosts = {
+      acer = {
         system = "x86_64-linux";
+        hardwareModules = [
+          nixos-hardware.nixosModules.common-cpu-intel
+          nixos-hardware.nixosModules.common-pc-ssd
+          nixos-hardware.nixosModules.common-pc-laptop
+        ];
+      };
+      xeon = {
+        system = "x86_64-linux";
+        hardwareModules = [
+          # Add xeon-specific hardware modules here
+          nixos-hardware.nixosModules.common-cpu-intel
+          nixos-hardware.nixosModules.common-pc-ssd
+        ];
+      };
+    };
+
+    # Generate NixOS configurations for all hosts
+    mkNixosConfiguration = hostname: hostConfig:
+      nixpkgs.lib.nixosSystem {
+        system = hostConfig.system;
         specialArgs = {
           inherit inputs nixpkgs-unstable;
         };
         modules = [
-          ./hosts/acer/configuration.nix
-          nixos-hardware.nixosModules.common-cpu-intel
-          nixos-hardware.nixosModules.common-pc-ssd
-          nixos-hardware.nixosModules.common-pc-laptop
-          nur.modules.nixos.default
-          sops-nix.nixosModules.sops
-          {
-            nixpkgs.overlays = [
-              nur.overlays.default
-              (final: prev: {
-                unstable = import nixpkgs-unstable {
-                  system = prev.system;
-                  config.allowUnfree = true;
-                };
-                weston = prev.weston.overrideAttrs (old: {
-                  mesonFlags = (old.mesonFlags or []) ++ ["-Dbackend-vnc=false"];
-                });
-              })
-            ];
-
-            nixpkgs.config = {
-              allowUnfree = true;
-              nvidia.acceptLicense = true;
-            };
-
-            nix.settings = {
-              substituters = [
-                "https://cache.nixos.org/"
-                "https://nix-community.cachix.org"
-                "https://hyprland.cachix.org"
-                "https://nixpkgs-unfree.cachix.org"
-              ];
-              trusted-public-keys = [
-                "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-                "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-                "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-                "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
-              ];
-            };
-          }
-        ];
+          ./hosts/${hostname}/configuration.nix
+        ] ++ hostConfig.hardwareModules ++ commonNixosModules;
       };
-    };
+
+  in {
+    nixosConfigurations = nixpkgs.lib.mapAttrs mkNixosConfiguration hosts;
 
     homeConfigurations = {
       xfeusw = home-manager.lib.homeManagerConfiguration {
@@ -119,25 +166,7 @@
         };
         modules = [
           ./home/xfeusw/home.nix
-          plasma-manager.homeModules.plasma-manager
-          nix-colors.homeManagerModules.default
-          niri.homeModules.niri
-          niri.homeManagerModules.default
-          sops-nix.homeModules.sops
-          spicetify-nix.homeManagerModules.default
-          {
-            nixpkgs.config.allowUnfree = true;
-            nixpkgs.overlays = [
-              nur.overlays.default
-              (final: prev: {
-                unstable = import nixpkgs-unstable {
-                  system = prev.system;
-                  config.allowUnfree = true;
-                };
-              })
-            ];
-          }
-        ];
+        ] ++ commonHomeModules;
       };
     };
   };
--- modules/security/default.nix
+++ modules/security/default.nix
@@ -148,4 +148,14 @@
       "-a always,exit -F arch=b64 -S execve -k exec"
     ];
   };
+
+  sops = {
+    defaultSopsFile = ../secrets/secrets.yaml;
+    defaultSopsFormat = "yaml";
+    age.keyFile = "${config.users.users.xfeusw.home}/.config/sops/age/keys.txt";
+    secrets.gh_token = {
+      owner = "xfeusw";
+      mode = "0400";
+    };
+  };
 }
